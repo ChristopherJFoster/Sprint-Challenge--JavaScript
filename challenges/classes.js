9975baf7a2e98689104bd8e4b4df904f8ea8f819
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-01-25 13:56:24 -0500

    Stretch: Completed stretch portion of classes.js. (Stretch portion of objects-arrays.js completed earlier.)

--- challenges/classes.js
+++ challenges/classes.js
@@ -26,3 +26,25 @@ console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+// The formulas for volume and surface area of a cube are the same as for a cuboid (a cube is just a special cuboid). I created two other methods instead:
+// 1. calcFaceDiag(), which calculates the length of the diagonal from a corner of the cube to any corner on the same side. The result will always be the same for a cube regardless of which two corners are chosen.
+// 2. calcMainDiag(), which calculates the length of the diagonal from any corner of the cube to the corner opposite of that corner (through the center of the cube). The result will always be the same for a cube regardless of which two corners are chosen.
+
+class CubeMaker extends CuboidMaker {
+  constructor(atts) {
+    super(atts);
+  }
+  calcFaceDiag() {
+    return Math.sqrt(2 * Math.pow(this.width, 2));
+  }
+  calcMainDiag() {
+    return this.width * Math.sqrt(3);
+  }
+}
+
+const cube = new CubeMaker({ length: 3, width: 3, height: 3 });
+console.log(cube.volume()); // 27 As mentioned above, the Cubiod volume() and surfaceArea() work fine.
+console.log(cube.surfaceArea()); // 54
+console.log(cube.calcFaceDiag()); // 4.242640687119285
+console.log(cube.calcMainDiag()); // 5.196152422706632
